a9e344a3db219e2e3ff72fd6d999cf836836c94b
kekonat <kekonat@gmail.com>
2016-03-09 01:51:03 -0800

    created skeleton for ReadThread as an inner class of TorRouter

--- Tor61Project/src/TorRouter.java
+++ Tor61Project/src/TorRouter.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.SocketException;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,11 +16,12 @@ public class TorRouter {
 	private ServerSocket SOCKET;
 	private TorRouterThread ROUTER;
 	
-	protected boolean LISTENING;
-	protected Map<RouterTableKey,Integer> router_table;
+	private boolean LISTENING;
+	private Map<RouterTableKey,Integer> router_table;
 
 	public TorRouter(ServerSocket socket) {
 		SOCKET = socket;
+		ROUTER = null;
 		LISTENING = false;
 		router_table = new HashMap<RouterTableKey,Integer>();
 	}
@@ -29,8 +31,9 @@ public class TorRouter {
 	 * @return returns true if successfully started, and false otherwise
 	 */
 	public boolean start() {
-		if (!LISTENING) {
+		if (!LISTENING && ROUTER == null) {
 			LISTENING = true;
+			ROUTER = new TorRouterThread(SOCKET);
 			return true;
 		} else {
 			System.out.println("Tor Router is already listening");
@@ -43,10 +46,21 @@ public class TorRouter {
 	 * @return true if successfully close the application, and false otherwise
 	 */
 	public boolean quit() {
-    	LISTENING = false;
     	System.out.println("Tor Router is Terminating. Please note that this operation can take up to 20 seconds");
+    	if (ROUTER == null) {
+    		System.out.println("ROUTER was null. Router never started");
+    		return false;
+    	}
+    	if (!LISTENING) {
+    		System.out.println("LISTENING was false. Router never started");
+    		return false;
+    	}
+    	LISTENING = false;
+    	
     	try {
+    		System.out.println("Attemping to Join Router...");
     		ROUTER.join();
+    		System.out.println("Join Router Success!");
     		return true;
     	} catch (InterruptedException e) {
     		e.printStackTrace();
@@ -62,7 +76,7 @@ public class TorRouter {
 	 * TorRouterThread Listens to incoming tcp connections, and handles it accordingly
 	 *
 	 */
-	public class TorRouterThread extends Thread {
+	private class TorRouterThread extends Thread {
 		private ServerSocket SOCKET;
 		
 		public TorRouterThread(ServerSocket socket) {
@@ -74,19 +88,28 @@ public class TorRouter {
 				// Set timeout to be 20 seconds
 				try {
 					SOCKET.setSoTimeout(20000);
-					// do something here
+					
+					Socket s = SOCKET.accept();
+					SOCKET.setSoTimeout(0); // Kill the timer
+					
+					// Create new thread to handle receiving messages
+					Thread read_thread = new ReadThread(s);
+					read_thread.start();
+					
 				} catch (SocketException e) {
 					System.out.println("SocketException when Tor Router is trying to create a bew tcp connection");
 					System.exit(1);
 				} catch (IOException e) {
+					// Socket Timeout Exceptions are caught here
+					// This is used to allow the thread to check if we are still LISTENING
 					continue;
 				}
 			}
+			// Being here means that we are no longer LISTENING, and we want to quit
 			try {
 				SOCKET.close();
 			} catch (IOException e) {
 				System.out.println("IOException: Tor Router no longer listening, but failed to close socket");
-				System.exit(1);
 			}
 		}
 	}
@@ -95,10 +118,40 @@ public class TorRouter {
 	 * 
 	 * @author Tyler
 	 * 
-	 * Inner class that creates a key for the router table
+	 * ReadThread reads incoming Tor Messages and decides how to handle each message
+	 * ReadThread continuously reads until either an end message it received, or we stop Listening
 	 *
 	 */
-	public class RouterTableKey {
+	private class ReadThread extends Thread {
+		
+		private Socket SOCKET;
+		
+		public ReadThread(Socket s) {
+			this.SOCKET = s;
+		}
+		
+		public void run() {
+			while (LISTENING) {
+				
+			}
+			
+			// Being here means that we are no longer LISTENING, and we want to quit
+			try {
+				SOCKET.close();
+			} catch (IOException e) {
+				System.out.println("IOException: ReadThread no longer listening, but failed to close socket");
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 * @author Tyler
+	 * 
+	 * Inner class that is used as a key for the router table
+	 *
+	 */
+	private class RouterTableKey {
 		
 		public ServerSocket socket;
 		public int circuit_id;
