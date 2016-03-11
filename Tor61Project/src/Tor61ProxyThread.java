b99ef5826e0d69557e179b606fb4ac6d0ec56438
Xin (Cyndi) Ai <aixin@cs.washington.edu>
2016-03-10 18:25:32 -0800

    taking out some warnings

--- Tor61Project/src/Tor61ProxyThread.java
+++ Tor61Project/src/Tor61ProxyThread.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ConnectException;
 import java.net.Socket;
-import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 
 
